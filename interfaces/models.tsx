32d6872b4e150257a9ff6add3b35167941a2c9fb
Hadrien <smethadrien.96@gmail.com>
2022-12-22 15:28:05 +0100

    Started so set the introduction section on the home page, Had a lot of trouble to handle the mousemove event with typescript hopefully someone created stackOverflow and chatGpt, I love them

---
+++ interfaces/models.tsx
@@ -0,0 +1,3 @@
+export interface SyntheticEvent {
+    e: React.MouseEvent<HTMLElement>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
-
+import Image from "next/image";
+import handsomeYoungDevelopper from "../assets/images/photo-cv_151222.jpg";
+import MuiGradientBorder from "../components/MuiGradientBorder";
 const index = () => {
-  return (
-    <div>
-      <h1>Hello NEXT</h1>
-    </div>
-  );
+    return (
+        <main>
+            <section className="intro">
+                <MuiGradientBorder>
+                    <div className="intro__content">
+                        <div className="intro__details">
+                            <h1>
+                                Hadrien Smet
+                                <br />
+                                <span>Front-end developer</span>
+                                <br />
+                                <strong>JavaScript</strong>
+                            </h1>
+                            <p>
+                                I help the companies to find a fine solution to
+                                deploy their services online
+                            </p>
+                        </div>
+                        <div className="intro__img-container">
+                            <Image
+                                src={handsomeYoungDevelopper}
+                                alt="Probablement l'un des meilleurs et l'un des plus beau développeurs. Mais loin d'être prétentieux."
+                                width={350}
+                                height={420}
+                            />
+                        </div>
+                    </div>
+                </MuiGradientBorder>
+            </section>
+        </main>
+    );
 };
 
 export default index;

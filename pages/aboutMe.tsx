a1cb37817bfed9f59f238609382fa299a505d136
Hadrien <smethadrien.96@gmail.com>
2022-12-22 12:00:52 +0100

    first commit

---
+++ pages/aboutMe.tsx
@@ -0,0 +1,9 @@
+const aboutMe = () => {
+    return (
+        <div>
+            <h1>About me</h1>
+        </div>
+    );
+};
+
+export default aboutMe;
---
+++ pages/aboutMyWork.tsx
@@ -0,0 +1,9 @@
+const aboutMyWork = () => {
+    return (
+        <div>
+            <h1>About my work</h1>
+        </div>
+    );
+};
+
+export default aboutMyWork;
